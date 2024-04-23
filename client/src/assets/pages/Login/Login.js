0b9277982b8b65dfd87e545d6797485a16f194be
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-04-23 12:13:01 -0400

    Partials: mixins, variables, fonts. Login page. Routes

--- client/src/assets/pages/Login/Login.js
+++ client/src/assets/pages/Login/Login.js
@@ -1,9 +1,24 @@
 import './Login.scss';
 
 function Login(){
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return(
-        <>
-        </>
+        <div className='login'>
+            <form className='form' onSubmit={handleSubmit}>
+                <h2 className='form__title'>Login</h2>
+                <label className='form__label' htmlFor="username">Username:</label>
+                <input className='form__input' type="text" id="username" name="username" required />
+                <br />
+                <label className='form__label' htmlFor="password">Password:</label>
+                <input className='form__input' type="password" id="password" name="password" required />
+                <br />
+                <button className='form__button' type="submit">Login</button>
+
+            </form>
+        </div>
     )
 }
 
