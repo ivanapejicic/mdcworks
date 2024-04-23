d050173343f15d4eb66352647c2795fd546e8a2b
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-04-23 15:12:47 -0400

    Home page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './assets/pages/Login/Login';
-
+import Home from './assets/pages/Home/Home';
 
 function App() {
   return (
@@ -9,6 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path= '/' element={<Login />} />
+          <Route path= '/home' element={<Home />} />
+
         </Routes>      
       </BrowserRouter>
     </div>
---
+++ client/src/assets/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.scss';
+
+function Home () {
+    return(
+        <div className='home'>
+            This is where students choose their major (category)
+            and from here they are being redirected to the list of jobs in
+            that category
+        </div>
+    );
+}
+
+export default Home;
