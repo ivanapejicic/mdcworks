7422167395ec05ca6a11fc29367a825175a56f92
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-05-04 23:49:52 -0400

    Added styling to Login form: icons, colors, fonts, and placeholders

--- client/src/assets/pages/Login/Login.js
+++ client/src/assets/pages/Login/Login.js
@@ -1,4 +1,6 @@
 import './Login.scss';
+import { FiUser } from "react-icons/fi";
+import { GoLock } from "react-icons/go";
 
 function Login(){
 
@@ -18,14 +20,18 @@ function Login(){
     return(
         <div className='login'>
             <form className='form' onSubmit={handleSubmit}>
-                <h2 className='form__title'>Login</h2>
-                <label className='form__label' htmlFor="username">Username:</label>
-                <input className='form__input' type="text" id="username" name="username" required />
+                <h2 className='form__title'>MDC Works Login</h2>
+                <div className='form__container'>
+                    <FiUser className='form__container-icon' />
+                    <input className='form__container-input' type="text" id="username" name="username" placeholder="Username" required />
+                </div>                
                 <br />
-                <label className='form__label' htmlFor="password">Password:</label>
-                <input className='form__input' type="password" id="password" name="password" required />
+                <div className='form__container'>
+                    <GoLock className='form__container-icon' />
+                    <input className='form__container-input' type="password" id="password" name="password" placeholder="Password" required />
+                </div>
                 <br />
-                <button className='form__button' type="submit">Login</button>
+                <button className='form__button' type="submit">LOGIN</button>
 
             </form>
         </div>
