14bbfa54f5877246a9ed1595438ca10cccda74ef
Olgica Zlatkovska <olgicazlatkovska@Olgicas-iMac.local>
2024-06-06 16:05:03 -0400

    add a database connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
+const PORT = process.env.PORT || 8080;
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose')
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://admin:9PIHj2w7lSQ8abYM@mdcworksbackend.3k2uqbc.mongodb.net/?retryWrites=true&w=majority&appName=MDCWorksBackend')
+        .then(() => {
+            console.log("Connected to the database")
+        })
+        .catch(() => {
+            console.log("Connection failed");
+        });
+
+app.get('/', (req, res) => {
+    res.send('Hello from home/updated')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
