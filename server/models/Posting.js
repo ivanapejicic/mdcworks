7eb789d699e0b93e13c7922cf6b1d148522ee5b4
Olgica Zlatkovska <olgicazlatkovska@Olgicas-iMac.local>
2024-06-06 16:33:26 -0400

    create a job posting schema

---
+++ server/models/Posting.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const PostingSchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: [true, 'Please enter a company name.']
+    },
+    companyLogo: {
+        type: String,
+        required: false
+    },
+    jobDescription: {
+        type: String,
+        required: [true, 'Please enter a job description.']
+    },
+    jobApplicationURL: {
+        type: String,
+        required: [true, 'Please enter the job application URL.'],
+        validate: {
+            validator: function (v) {
+                return /^(ftp|http|https):\/\/[^ "]+$/.test(v);
+            },
+            message: 'Please enter a valid URL.'
+        }
+    },
+    isExpired: {
+        type: Boolean,
+        required: [true, 'Please specify if the job posting is still active.']
+    },
+    jobType: {
+        type: String,
+        required: [true, 'Please enter the job type.']
+    },
+    jobLocation: {
+        type: String,
+        required: [true, 'Please enter the job location.']
+    },
+    positionName: {
+        type: String,
+        required: [true, 'Please enter the position name.']
+    },
+    jobPostingDate: {
+        type: Date,
+        required: [true, 'Please enter the job posting date.']
+    },
+    jobRating: {
+        type: Number,
+        required: [true, 'Please enter the company rating.']
+    },
+    positionSalary: {
+        type: Number,
+        required: false
+    }},
+    {
+        timestamps: true
+    });
+
+const Posting = mongoose.model('Posting', PostingSchema);
+
+module.exports = Posting;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
 const PORT = process.env.PORT || 8080;
 
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose')
 
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect('mongodb+srv://admin:9PIHj2w7lSQ8abYM@mdcworksbackend.3k2uqbc.mongodb.net/?retryWrites=true&w=majority&appName=MDCWorksBackend')
-        .then(() => {
-            console.log("Connected to the database")
-        })
-        .catch(() => {
-            console.log("Connection failed");
-        });
 
 app.get('/', (req, res) => {
     res.send('Hello from home/updated')
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.post('/api/postings', (req, res) => {
+    res.send('Data received')
+})
+
+mongoose.connect('mongodb+srv://admin:9PIHj2w7lSQ8abYM@mdcworksbackend.3k2uqbc.mongodb.net/?retryWrites=true&w=majority&appName=MDCWorksBackend')
+        .then(() => {
+            console.log("Connected to the database")
+            app.listen(PORT, () => {
+                console.log(`Server is running on port ${PORT}`);
+            });
+        })
+        .catch(() => {
+            console.log("Connection failed");
+        });
+
+
+
