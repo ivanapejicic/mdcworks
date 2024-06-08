a0abc27b81287c0f344fa26907f207cab2b282fe
Olgica Zlatkovska <olgicazlatkovska@Olgicas-iMac.local>
2024-06-08 15:46:24 -0400

    style for desktop

--- client/src/assets/components/Header/Header.js
+++ client/src/assets/components/Header/Header.js
@@ -1,35 +1,36 @@
 import './Header.scss';
-import logo from '../../images/mdcworks-logo.png';
-import { RxHamburgerMenu } from "react-icons/rx";
-import { Link } from "react-router-dom";
-import { useState } from 'react';
+import logo from './../../images/shark-tank.webp';
+import { navOptions } from '../../data/data';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
 
-    const [isMenuOpen, setIsMenuOpen] = useState(true);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
-
     return (
         <header className='header'>
-            <div className='header__logo-container'>
-                <img className='header__logo' src={logo} alt='logo for MDC Works' />
-            </div>
-            <div
-                className='header__menu-toggle'
-                onClick={toggleMenu}
-            >
-                <RxHamburgerMenu />
-            </div>
-            <nav className={`header__nav ${(isMenuOpen) ? 'open' : 'closed'}`}>
-                <Link className='header__nav-link' to='https://www.mdc.edu/mdcworks/explore-careers/default.aspx'>CAREERS</Link>
-                <Link className='header__nav-link' to='https://www.mdc.edu/mdcworks/career-resources/default.aspx'>RESOURCES</Link>
-                <Link className='header__nav-link' to='https://www.mdc.edu/mdcworks/events.aspx'>EVENTS</Link>
-                <Link className='header__nav-link' to='https://www.mdc.edu/mdcworks/employers.aspx'>EMPLOYERS</Link>
-                <Link className='header__nav-link' to='https://www.mdc.edu/mdcworks/contact.aspx'>CONTACT</Link>
+            <a className='header__image-container' href='/'>
+                <img className='header__image' src={logo} alt='placeholder' />
+            </a>
+            <nav className='header__nav'>
+                <ul>
+                    {
+                        navOptions.map((item) => (
+                            <li className='header__nav-item'>
+                                <NavLink to={`/${item.path}`}>
+                                    {item.name}
+                                </NavLink>
+                            </li>
+                        ))
+                    }
+                </ul>
             </nav>
+            <div className='header__buttons-container'>
+                <button className='header__login'>
+                    <a href='/login'>Log In</a>
+                </button>
+                <button className='header__signup'>
+                    <a href='https://www.mdc.edu/registration/'>Sign Up</a>
+                </button>
+            </div>
         </header>
     )
 }
