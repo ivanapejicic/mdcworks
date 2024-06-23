1b52111d73730397ae22bffee8634b8399b7acc5
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-06-23 02:10:57 -0400

    Company details form added for admin

--- client/src/assets/pages/Admin/Admin.js
+++ client/src/assets/pages/Admin/Admin.js
@@ -1,46 +1,51 @@
 import './Admin.scss';
 import HeaderAdmin from '../../components/HeaderAdmin/HeaderAdmin';
+import { TiUpload } from "react-icons/ti";
 import { IoBagRemoveOutline } from "react-icons/io5";
 import { GrGroup } from "react-icons/gr";
 import { CiCirclePlus } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
+import { FaUpload } from "react-icons/fa"; 
 
 function Admin() {
     return (
-        <div className='admin-main'>
+        <div className='admin'>
             <HeaderAdmin />
-            <div className='admin'>
-                <p className='admin__text'>Signed in as Admin</p>
-                <h3 className='admin__welcome'>WELCOME CHRISTIAN ORTEGA</h3>
-            </div>
-            <div className='stats'>
-                <div className='stats__jobs'>
-                    <div className='stats__jobs-left'>
-                        <p>10</p>
-                        <p className='stats__jobs-left__text'>Open Jobs</p>
+            <div className='admin__body'>
+                <h2 className='admin__body-title'>Add New Job</h2>
+
+                <div className='company'>
+                    <h3 className='company__title'>Company Details</h3>
+                    <p className='company__text'>Please share additional details about company.</p>
+                    <div className='company__form'>
+                        <div className='company__form-top'>
+                            <div className='field'>
+                                <label className='field__label' htmlFor="companyname">Company Name *</label>
+                                <input className='field__input' type="text" id="companyname" name="companyname" placeholder="Apple LLC" required />
+                            </div>
+                            <div className='field'>
+                                <label className='field__label' htmlFor="website">Website</label>
+                                <input className='field__input' type="text" id="website" name="website" placeholder="www.example.com" required />
+                            </div>
+                        </div>
+                        <div className='company__form-bottom'>
+                        <div className='company__form-bottom'>
+                            <div className='field'>
+                                <label className='field__label' htmlFor="companylogo">Company Logo</label>
+                                {/* <input className='field__upload'type="file" id="companylogo" name="companylogo" accept="image/png, image/jpeg, image/jpg" multiple /> */}
+                                <div
+                                    className='field__upload'
+                                    ondrop="dropHandler(event);"
+                                    ondragover="dragOverHandler(event);">
+                                    <TiUpload style={{ width: '2rem', height: '4rem', margin: '.2rem'}}/>
+                                    <p className='text'><b>Drop your files here</b></p>
+                                    <p className='text'>or click to choose from your device</p>
+                                </div>
+                                <button className='field__button'>Upload file</button>
+                            </div>
+                        </div>
+                        </div>
                     </div>
-                    <div className='stats__jobs-right'>
-                        <IoBagRemoveOutline />
-                    </div>
-                </div>
-                <div className='stats__students'>
-                    <div className='stats__students-left'>
-                        <p>850</p>
-                        <p className='stats__students-left__text'>Saved Students</p>
-                    </div>
-                    <div className='stats__students-right'>
-                        <GrGroup />
-                    </div>
-                </div>
-            </div>
-            <div className='options'>
-                <div className='options__add'>
-                    <CiCirclePlus className='options__add-icon' />
-                    <p className='options__add-text'>ADD NEW JOB</p>
-                </div>
-                <div className='options__edit'>
-                    <CiEdit className='options__edit-icon' />
-                    <p className='options__add-text'>EDIT JOB LISTING</p>
                 </div>
             </div>
         </div>
